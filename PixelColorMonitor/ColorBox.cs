197f3f91986dffa583cbfe3a92353b9cb0f42845
shir0areed <38010555+shir0areed@users.noreply.github.com>
2021-09-24 12:53:32 +1100

    補間しないように

--- PixelColorMonitor/ColorBox.cs
+++ PixelColorMonitor/ColorBox.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,9 @@ namespace PixelColorMonitor
 
         protected override void OnPaint(PaintEventArgs pe)
         {
+            pe.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            pe.Graphics.PixelOffsetMode = PixelOffsetMode.Half;
+
             base.OnPaint(pe);
         }
     }
