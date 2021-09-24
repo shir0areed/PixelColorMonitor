c6d480702314279490c9ef9be12bd969caecac45
shir0areed <38010555+shir0areed@users.noreply.github.com>
2021-09-24 12:22:55 +1100

    識別子変更

--- PixelColorMonitor/PixelColorMonitorForm.Designer.cs
+++ PixelColorMonitor/PixelColorMonitorForm.Designer.cs
@@ -30,8 +30,8 @@ namespace PixelColorMonitor
         private void InitializeComponent()
         {
             this.PickButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ColorBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).BeginInit();
             this.SuspendLayout();
             // 
             // PickButton
@@ -45,28 +45,28 @@ namespace PixelColorMonitor
             this.PickButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PickButton_MouseMove);
             this.PickButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PickButton_MouseUp);
             // 
-            // pictureBox1
+            // ColorBox
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(99, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.ColorBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorBox.Location = new System.Drawing.Point(99, 13);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(40, 40);
+            this.ColorBox.TabIndex = 1;
+            this.ColorBox.TabStop = false;
             // 
             // PixelColorMonitorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ColorBox);
             this.Controls.Add(this.PickButton);
             this.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "PixelColorMonitorForm";
             this.Text = "PixelColorMonitor";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -74,7 +74,7 @@ namespace PixelColorMonitor
         #endregion
 
         private System.Windows.Forms.Button PickButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox ColorBox;
     }
 }
 
