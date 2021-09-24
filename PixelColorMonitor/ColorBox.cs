bc4a2588d2cd6f9c7ff8a1fe529125fe128e6796
shir0areed <38010555+shir0areed@users.noreply.github.com>
2021-09-24 12:51:33 +1100

    PictureBox継承

--- PixelColorMonitor/ColorBox.cs
+++ PixelColorMonitor/ColorBox.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace PixelColorMonitor
 {
-    public partial class ColorBox : Control
+    public partial class ColorBox : PictureBox
     {
         public ColorBox()
         {
